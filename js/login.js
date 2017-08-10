aaea3d8db124161100d4f8f04ea000d95805c2ce
Sean McPherson <seanmcp25@gmail.com>
2017-08-10 15:50:41 -0400

    Login page created with basic js functionality

--- js/login.js
+++ js/login.js
@@ -1,14 +1,64 @@
-let fetchOps = {
-  url: 'url'
+function clearBody(){
+  document.body.innerHTML = "";
 }
 
-fetch(fetchOps)
-  .then(function(res){
-    if(res.status < 200 || res.status >= 300){
-      console.log("Status: ", res.status);
+function loadLogin(){
+  clearBody();
+  document.body.innerHTML +=
+  `
+  <section id="loginContainer">
+    <h1>Iron Meetups!</h1>
+    <h3>Please Sign Up!</h3>
+    <form>
+      <input id="loginEmail" type="text" name="email" placeholder="Email Address" required>
+      <input id="loginPassword" type="text" name="password" placeholder="Password" required>
+      <button id="loginSignUp" type="button" name="signUp">Sign Up!</button>
+      <button id="loginLogin" class="active" type="button" name="login">Login</button>
+    </form>
+  </section>
+  <script type="text/javascript" src="../js/login.js"></script>
+  `
+  console.log('loadLogin() successful');
+}
+
+loadLogin();
+
+let loginEmail = document.getElementById('loginEmail');
+let loginPassword = document.getElementById('loginPassword');
+
+let loginLogin = document.getElementById('loginLogin');
+let loginSignUp = document.getElementById('loginSignUp');
+
+loginLogin.addEventListener('click', function(){
+  if(!loginEmail.value || !loginPassword.value) {
+    alert("Please provide email and password");
+  } else {
+    console.log('True');
+    console.log('Email: ', loginEmail.value);
+    console.log('Password: ', loginPassword.value);
+  }
+})
+
+loginSignUp.addEventListener('click', function(){
+  // Run loadSignUp();
+  console.log('Run loadSignUp();');
+})
+
+function loginCheck(){
+  let loginFetch = {
+    body: {
+      'email': document.getElementById('loginEmail').value,
+      'password': document.getElementById('loginPassword').value
+    },
+    method: 'post',
+    url: '/api/user/login'
+  }
+  fetch(loginFetch)
+  .then(function(response){
+    if(response.status === 200){
+      // loadEventList();
+    } else {
+      alert("Invalid email and/or password. Please try again.")
     }
-    // Main function
   })
-  .catch(function(err){
-    console.log("Error: ", err);
-  })
+}

cc2e10db4c24dbfed55164450fbd1b4eaa21f669
Joseph Gordy <jgordy2424@gmail.com>
2017-08-10 16:31:38 -0400

    Changed single event cards to take up less space on the page. Added fetch statment with placeholder URL

--- js/eventlist.js
+++ js/eventlist.js
@@ -4,7 +4,7 @@ let body = document.getElementsByTagName("body");
 
 
 function loadEventList() {
-  
+
   document.body.innerHTML += `<header id="nav_bar">
                                 <h2 id="logo">Iron Meetups</h2>
                                 <nav id="nav_links">
@@ -15,58 +15,45 @@ function loadEventList() {
                               <main id="main_content">
                               </main>`;
 
-  for (var i = 0; i < results.length; i++) {
+  fetch("url")
+    .then(
+      function(response) {
+        if (response.status !== 200) {
+          console.log(response.status);
+          return;
+        }
+        response.json().then(function(data) {
+          console.log("Here is the data:", data);
 
-    let mainContent = document.getElementById("main_content");
+          for (var i = 0; i < results.length; i++) {
 
-    mainContent.innerHTML += `
-                            <article class="each_container">
-                              <div class="event_details">
-                                <h2>${results[i].data.name}</h2>
-                                <p>${results[i].data.city}, ${results[i].data.state}</p>
-                                <p>${results[i].data.date} - ${results[i].data.time}</p>
-                              </div>
-                              <div class="button_container">
-                                <div class="event_buttons">Click to RSVP</div>
-                                <div class="event_buttons">View Event Info</div>
-                              </div>
-                            </article>
-                            `
-  }
+            let mainContent = document.getElementById("main_content");
+
+            mainContent.innerHTML += `
+                                    <article class="each_container">
+                                      <div class="event_details">
+                                        <h2>${results[i].data.name}</h2>
+                                        <p>${results[i].data.city}, ${results[i].data.state}</p>
+                                        <p>${results[i].data.date} - ${results[i].data.time}</p>
+                                      </div>
+                                      <div class="button_container">
+                                        <div class="event_buttons">Click to RSVP</div>
+                                        <div class="event_buttons">View Event Info</div>
+                                      </div>
+                                    </article>
+                                    `
+          }
+
+        });
+      }
+    )
+    .catch(function(err) {
+      console.log("Fetch Error :-S", err);
+    });
+
 
 
 
 }
 
 loadEventList();
-
-
-
-
-
-
-
-
-
-
-// fetch("url")
-//   .then(
-//     function(response) {
-//       if (response.status !== 200) {
-//         console.log(response.status);
-//         return;
-//       }
-//       response.json().then(function(data) {
-//         console.log("Here is the data:", data);
-//
-//         for (var i = 0; i < results.length; i++) {
-//
-//
-//
-//         }
-//       });
-//     }
-//   )
-//   .catch(function(err) {
-//     console.log("Fetch Error :-S", err);
-//   });

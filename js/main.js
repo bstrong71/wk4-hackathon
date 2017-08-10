38f74b4ad38564554acd06861666b88d5aa3f5b6
Sean McPherson <seanmcp25@gmail.com>
2017-08-09 20:36:12 -0400

    Modified main.js: Added sample results object array; commented out boilerplate fetch

--- js/main.js
+++ js/main.js
@@ -1,14 +1,128 @@
-let fetchOps = {
-  url: 'url'
-}
-
-fetch(fetchOps)
-  .then(function(res){
-    if(res.status < 200 || res.status >= 300){
-      console.log("Status: ", res.status);
+let results = [{
+    'status': 'true',
+    'data': {
+      'name': 'Event Name',
+      'date': '08/10/2017',
+      'time': '1800',
+      'address': '1400 MLK Jr. Blvd',
+      'city': 'Atalanta',
+      'state': 'GA',
+      'details': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      'attendees': [{
+        'name': 'First Lastname',
+        'username': 'userName',
+        'avatar_url': 'https://avatars2.githubusercontent.com/u/125464?v=4&s=88'
+      },
+      {
+        'name': 'Second Lastname',
+        'username': 'user_name',
+        'avatar_url': 'https://avatars0.githubusercontent.com/u/8208326?v=4&s=88'
+      },
+      {
+        'name': 'Third Lastname',
+        'username': 'USER NAME',
+        'avatar_url': 'https://avatars6.githubusercontent.com/u/464297?v=4&s=88'
+      }
+      ]
     }
-    // Main function
-  })
-  .catch(function(err){
-    console.log("Error: ", err);
-  })
+  },
+  {
+    'status': 'true',
+    'data': {
+      'name': 'Event Name',
+      'date': '08/11/2017',
+      'time': '1830',
+      'address': '3000 Peachtree St',
+      'city': 'Atalanta',
+      'state': 'GA',
+      'details': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      'attendees': [{
+        'name': 'First Lastname',
+        'username': 'userName',
+        'avatar_url': 'https://avatars2.githubusercontent.com/u/125464?v=4&s=88'
+      },
+      {
+        'name': 'Second Lastname',
+        'username': 'user_name',
+        'avatar_url': 'https://avatars0.githubusercontent.com/u/8208326?v=4&s=88'
+      },
+      {
+        'name': 'Third Lastname',
+        'username': 'USER NAME',
+        'avatar_url': 'https://avatars6.githubusercontent.com/u/464297?v=4&s=88'
+      }
+      ]
+    }
+  },
+  {
+    'status': 'true',
+    'data': {
+      'name': 'Event Name',
+      'date': '08/12/2017',
+      'time': '1900',
+      'address': '545 Old Macon Rd',
+      'city': 'Atalanta',
+      'state': 'GA',
+      'details': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      'attendees': [{
+        'name': 'First Lastname',
+        'username': 'userName',
+        'avatar_url': 'https://avatars2.githubusercontent.com/u/125464?v=4&s=88'
+      },
+      {
+        'name': 'Second Lastname',
+        'username': 'user_name',
+        'avatar_url': 'https://avatars0.githubusercontent.com/u/8208326?v=4&s=88'
+      },
+      {
+        'name': 'Third Lastname',
+        'username': 'USER NAME',
+        'avatar_url': 'https://avatars6.githubusercontent.com/u/464297?v=4&s=88'
+      }
+      ]
+    }
+  },
+  {
+    'status': 'true',
+    'data': {
+      'name': 'Event Name',
+      'date': '08/13/2017',
+      'time': '1930',
+      'address': '2400 Ponce De Leon Ave NW',
+      'city': 'Atalanta',
+      'state': 'GA',
+      'details': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      'attendees': [{
+        'name': 'First Lastname',
+        'username': 'userName',
+        'avatar_url': 'https://avatars2.githubusercontent.com/u/125464?v=4&s=88'
+      },
+      {
+        'name': 'Second Lastname',
+        'username': 'user_name',
+        'avatar_url': 'https://avatars0.githubusercontent.com/u/8208326?v=4&s=88'
+      },
+      {
+        'name': 'Third Lastname',
+        'username': 'USER NAME',
+        'avatar_url': 'https://avatars6.githubusercontent.com/u/464297?v=4&s=88'
+      }
+      ]
+    }
+  }
+]
+
+// let fetchOps = {
+//   url: 'url'
+// }
+//
+// fetch(fetchOps)
+//   .then(function(res){
+//     if(res.status < 200 || res.status >= 300){
+//       console.log("Status: ", res.status);
+//     }
+//     // Main function
+//   })
+//   .catch(function(err){
+//     console.log("Error: ", err);
+//   })
